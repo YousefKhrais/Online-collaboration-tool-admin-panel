00efbd414488d54435a82775cda80f3adbff6765
Yousef Khrais <yousefokhrais@gmail.com>
2022-07-17 13:52:56 +0300

    Added middleware auth for protected routes

--- routes/web.php
+++ routes/web.php
@@ -29,43 +29,43 @@ Route::get('/dashboard/profile', [DashboardController::class, 'profile'])->middl
 
 Route::prefix('/teacher')->group(function () {
     Route::get('/', [TeachersController::class, 'index'])->middleware(['auth'])->name('teachers');
-    Route::post('/store', [TeachersController::class, 'store'])->name('teacher.store');
-    Route::get('/view/{id}', [TeachersController::class, 'view']);
-    Route::post('/update/{id}', [TeachersController::class, 'update']);
-    Route::post('/delete/{id}', [TeachersController::class, 'destroy']);
+    Route::post('/store', [TeachersController::class, 'store'])->middleware(['auth'])->name('teacher.store');
+    Route::get('/view/{id}', [TeachersController::class, 'view'])->middleware(['auth']);
+    Route::post('/update/{id}', [TeachersController::class, 'update'])->middleware(['auth']);
+    Route::post('/delete/{id}', [TeachersController::class, 'destroy'])->middleware(['auth']);
 });
 
 Route::prefix('/course')->group(function () {
     Route::get('/', [CoursesController::class, 'index'])->middleware(['auth'])->name('courses');
-    Route::post('/store', [CoursesController::class, 'store'])->name('course.store');
-    Route::get('/view/{id}', [CoursesController::class, 'view']);
-    Route::post('/enroll/{id}', [CoursesController::class, 'enroll']);
-    Route::post('/unenroll/{id}', [CoursesController::class, 'unenroll']);
-    Route::post('/update/{id}', [CoursesController::class, 'update']);
-    Route::post('/delete/{id}', [CoursesController::class, 'destroy']);
+    Route::post('/store', [CoursesController::class, 'store'])->middleware(['auth'])->name('course.store');
+    Route::get('/view/{id}', [CoursesController::class, 'view'])->middleware(['auth']);
+    Route::post('/enroll/{id}', [CoursesController::class, 'enroll'])->middleware(['auth']);
+    Route::post('/unenroll/{id}', [CoursesController::class, 'unenroll'])->middleware(['auth']);
+    Route::post('/update/{id}', [CoursesController::class, 'update'])->middleware(['auth']);
+    Route::post('/delete/{id}', [CoursesController::class, 'destroy'])->middleware(['auth']);
 });
 
 Route::prefix('/category')->group(function () {
     Route::get('/', [CategoriesController::class, 'index'])->middleware(['auth'])->name('categories');
-    Route::post('/store', [CategoriesController::class, 'store'])->name('category.store');
-    Route::get('/view/{id}', [CategoriesController::class, 'view']);
-    Route::post('/update/{id}', [CategoriesController::class, 'update']);
-    Route::post('/delete/{id}', [CategoriesController::class, 'destroy']);
+    Route::post('/store', [CategoriesController::class, 'store'])->middleware(['auth'])->name('category.store');
+    Route::get('/view/{id}', [CategoriesController::class, 'view'])->middleware(['auth']);
+    Route::post('/update/{id}', [CategoriesController::class, 'update'])->middleware(['auth']);
+    Route::post('/delete/{id}', [CategoriesController::class, 'destroy'])->middleware(['auth']);
 });
 
 Route::prefix('/student')->group(function () {
     Route::get('/', [StudentsController::class, 'index'])->middleware(['auth'])->name('students');
-    Route::post('/store', [StudentsController::class, 'store'])->name('student.store');
-    Route::get('/view/{id}', [StudentsController::class, 'view']);
-    Route::post('/update/{id}', [StudentsController::class, 'update']);
-    Route::post('/delete/{id}', [StudentsController::class, 'destroy']);
+    Route::post('/store', [StudentsController::class, 'store'])->middleware(['auth'])->name('student.store');
+    Route::get('/view/{id}', [StudentsController::class, 'view'])->middleware(['auth']);
+    Route::post('/update/{id}', [StudentsController::class, 'update'])->middleware(['auth']);
+    Route::post('/delete/{id}', [StudentsController::class, 'destroy'])->middleware(['auth']);
 });
 
 Route::prefix('/request')->group(function () {
     Route::get('/', [RequestsController::class, 'index'])->middleware(['auth'])->name('requests');
-    Route::get('/view/{id}', [RequestsController::class, 'view']);
-    Route::post('/accept/{id}', [RequestsController::class, 'accept']);
-    Route::post('/reject/{id}', [RequestsController::class, 'reject']);
+    Route::get('/view/{id}', [RequestsController::class, 'view'])->middleware(['auth']);
+    Route::post('/accept/{id}', [RequestsController::class, 'accept'])->middleware(['auth']);
+    Route::post('/reject/{id}', [RequestsController::class, 'reject'])->middleware(['auth']);
 });
 
 require __DIR__ . '/auth.php';
